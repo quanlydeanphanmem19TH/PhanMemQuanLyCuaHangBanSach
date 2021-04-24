429029a5a336f8a8bd3802baa06bc337186f42cd
= <=>
2021-04-24 08:03:08 +0700

    Hoang Danh Upluoad

---
+++ J_Unit_Github/src/FullTest.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+
+@RunWith(Suite.class)
+@SuiteClasses({CalcTest.class, StudentTest.class})
+public class FullTest {
+
+}
